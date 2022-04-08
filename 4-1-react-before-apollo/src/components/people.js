32841a6eaf134237a846e4a7a5d64d3a63aa2b45
YunDayoung <dyyoon@kci.co.kr>
2022-04-08 17:10:45 +0900

    4-1, error data가 null임

--- 4-1-react-before-apollo/src/components/people.js
+++ 4-1-react-before-apollo/src/components/people.js
@@ -1,25 +1,20 @@
-import './components.css';
+import "./components.css";
 
 function People() {
+  function AsideItems() {
+    return <div></div>;
+  }
 
-    function AsideItems () {
-        return (<div></div>);
-    }
+  function MainContents() {
+    return <div></div>;
+  }
 
-    function MainContents () {
-        return (<div></div>);
-    }
-
-    return (
-        <div id="people" className="component">
-            <aside>
-                {AsideItems()}
-            </aside>
-            <section className="contents">
-                {MainContents()}
-            </section>
-        </div>
-    )
+  return (
+    <div id="people" className="component">
+      <aside>{AsideItems()}</aside>
+      <section className="contents">{MainContents()}</section>
+    </div>
+  );
 }
 
 export default People;
